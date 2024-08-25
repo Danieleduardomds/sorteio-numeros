761ba50b8002041efb158989655daafa158e722d
daniel <ava540@einstein.br>
2024-08-25 00:31:25 -0300

    Adicionando o Pulsar no  resultado

--- src/app/sorteio/sorteio.component.ts
+++ src/app/sorteio/sorteio.component.ts
@@ -8,14 +8,17 @@ import { Component } from '@angular/core';
 export class SorteioComponent {
   numeroSorteado: number | null = null;
   loading: boolean = false;
+  showPulsarEffect = false;
 
   realizarSorteio(): void {
     this.loading = true; // Inicia a animação de carregamento
+    this.showPulsarEffect = false; // Garantir que a animação não seja repetida imediatamente
 
     // Simula o atraso do sorteio
     setTimeout(() => {
       this.numeroSorteado = Math.floor(Math.random() * 100) + 1; // Gera um número aleatório
       this.loading = false; // Remove a animação de carregamento
+      this.showPulsarEffect = true; // Adiciona o efeito pulsar após o sorteio
     }, 5000); // 5 segundos
   }
 }
